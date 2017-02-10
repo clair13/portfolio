9d5a567e55e12209ad22599a294222c7ae3279c6
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-10 20:57:19 +0100

    Integrated new and create actions for Ports

--- app/controllers/ports_controller.rb
+++ app/controllers/ports_controller.rb
@@ -2,4 +2,20 @@ class PortsController < ApplicationController
   def index
     @port_items = Port.all
   end
+
+  def new
+    @port_item = Port.new
+  end
+
+  def create
+    @port_item = Port.new(params.require(:port).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @port_item.save
+        format.html { redirect_to ports_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
 end
