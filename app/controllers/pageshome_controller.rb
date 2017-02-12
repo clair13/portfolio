db48f27b8a5c623a36a451723be7909b4fe20840
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-12 16:03:13 +0100

    Added new and edit buttons for port items

--- app/controllers/pageshome_controller.rb
+++ app/controllers/pageshome_controller.rb
@@ -1,4 +1,10 @@
-class PageshomeController < ApplicationController
+class PagesController < ApplicationController
+
+  def home
+    @posts = Blog.all
+    @skills = Skill.all
+  end
+
   def about
   end
 
