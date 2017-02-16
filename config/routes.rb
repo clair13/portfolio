6db3d6ab83839ffcd23b72c6cf54ca819608990c
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-16 16:03:55 +0100

    Customize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :ports, except: [:show]
   get 'angular-items', to: 'ports#angular'
   get 'port/:id', to: 'ports#show', as: 'port_show'
