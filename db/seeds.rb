3561dc61bb62c0d0ab4bef8b2c9b988e6f9e3144
clair13 <theclairvoyant1313@yahoo.gr>
2017-02-25 22:59:47 +0100

    Integrated Faker gem and implemented custom fonts

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,8 +51,8 @@ puts "5 skills created"
     title: "Portfolio title: #{port_item}",
     subtitle: "Ruby on Rails",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
+    main_image: Faker::Placeholdit.image("600x400"),
+    thumb_image: Faker::Placeholdit.image("350x200")
   )
 end
 
@@ -61,8 +61,8 @@ end
     title: "Portfolio title: #{port_item}",
     subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    main_image: "http://placehold.it/600x400",
-    thumb_image: "http://placehold.it/350x200"
+    main_image: Faker::Placeholdit.image("600x400"),
+    thumb_image: Faker::Placeholdit.image("350x200"),
   )
 end
 
